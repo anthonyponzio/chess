120e4cdcc5b9b954574220f0613c9f8108d86a35
anthony ponzio <anthonyponzio1992@gmail.com>
2020-02-08 17:34:41 -0500

    add Board#initialize, make @rows for testing

--- board.rb
+++ board.rb
@@ -1,2 +1,10 @@
+require_relative "piece"
+
 class Board
+  def initialize
+    @rows = Array.new(8) do |row|
+      home_row = [0, 1, 6, 7].include?(row) 
+      Array.new(8) { home_row ? Piece.new : nil }
+    end
+  end
 end
