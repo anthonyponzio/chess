5b76bdf6e23a16b4d961f3d3d0e24c03ef573067
anthony ponzio <anthonyponzio1992@gmail.com>
2020-02-09 11:23:48 -0500

    add #to_s and #symbol to Piece class

--- piece.rb
+++ piece.rb
@@ -4,4 +4,12 @@ class Piece
   def initialize(color, board, pos)
     @color, @board, @pos = color, board, pos
   end
+
+  def to_s
+    "#{symbol}"
+  end
+
+  def symbol
+    raise '#symbol needs to be implemented by subclass'
+  end
 end
