f8417a26b10c247023648ac8f69419d560f50fa8
anthony ponzio <anthonyponzio1992@gmail.com>
2020-02-09 12:15:02 -0500

    include Slideable in Bishop and add Bishop#move_dirs

--- pieces/bishop.rb
+++ pieces/bishop.rb
@@ -1,7 +1,14 @@
-require_relative "../piece.rb"
+require_relative "../piece"
+require_relative "./slideable"
 
 class Bishop < Piece
+  include Slideable
+
   def symbol
     :B
+  end
+
+  def move_dirs
+    diagonal_dirs
   end
 end
