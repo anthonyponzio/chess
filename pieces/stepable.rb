f01ac228a7b4154042ac0d76bb9babb7f7f8a36d
anthony ponzio <anthonyponzio1992@gmail.com>
2020-02-09 16:54:43 -0500

    add Stepable#moves

--- pieces/stepable.rb
+++ pieces/stepable.rb
@@ -1,2 +1,10 @@
 module Stepable
+  def moves
+    row, col = pos
+    moves = move_diffs.map { |(dx, dy)| [row + dx, col + dy] }
+
+    moves.select do |move|
+      board.valid_pos?(move) && (board[move].color != color)
+    end
+  end
 end
