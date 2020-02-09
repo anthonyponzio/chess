e60a01853e6f568396fc423a82ef1d9df0a576f7
anthony ponzio <anthonyponzio1992@gmail.com>
2020-02-09 16:30:23 -0500

    add Knight#move_diffs

--- knight.rb
+++ knight.rb
@@ -4,8 +4,20 @@ require_relative "stepable"
 class Knight < Piece
   include Stepable
 
-
   def symbol
     (color == :white) ? "♘" : "♞"
+  end
+
+  def move_diffs
+    [
+      [-2, -1], # up-left
+      [-2, 1],  # up-right
+      [2, -1],  # down-left
+      [2, 1],   # down-right
+      [-1,-2],  # left-up
+      [1, -2],  # left-down
+      [-1, 2],  # right-up
+      [1, 2],   # right-down
+    ]
   end
 end
