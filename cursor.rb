3bb1b92970688d28745555ecb9ed960198674557
anthony ponzio <anthonyponzio1992@gmail.com>
2020-02-10 17:50:51 -0500

    add selected tracking to Cursor class and Cursor#toggle_selected

--- cursor.rb
+++ cursor.rb
@@ -32,11 +32,12 @@ MOVES = {
 
 class Cursor
 
-  attr_reader :cursor_pos, :board
+  attr_reader :cursor_pos, :board, :selected
 
   def initialize(cursor_pos, board)
     @cursor_pos = cursor_pos
     @board = board
+    @selected = false
   end
 
   def get_input
@@ -75,9 +76,14 @@ class Cursor
     return input
   end
 
+  def toggle_selected
+    @selected = !@selected
+  end
+
   def handle_key(key)
     case key
     when :space, :return
+      toggle_selected
       return cursor_pos
     when :up, :down, :left, :right
       update_pos(MOVES[key])
