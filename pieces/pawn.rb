ddd643e1f90b8ff797d6af5fc8b25175368f4a5f
anthony ponzio <anthonyponzio1992@gmail.com>
2020-02-09 21:04:00 -0500

    add Pawn#moves

--- pieces/pawn.rb
+++ pieces/pawn.rb
@@ -5,6 +5,29 @@ class Pawn < Piece
     (color == :white) ? "♙" : "♟"
   end
 
+  def moves
+    moves = []
+    
+    row, col = pos
+    side_attacks.each do |(dx, dy)|
+      attack_pos = [row+dx, col+dy]
+      next unless board.valid_pos?(attack_pos)
+      piece = board[attack_pos]
+      if !piece.empty? && piece.color != color
+        moves << attack_pos
+      end
+    end
+
+    forward_steps.each do |(dx, dy)|
+      step_pos = [row+dx, col+dy]
+      if board.valid_pos?(step_pos) && board[step_pos].empty? 
+        moves << step_pos
+      end
+    end
+
+    moves
+  end
+
   private
   def at_start_row?
     row,_ = pos
