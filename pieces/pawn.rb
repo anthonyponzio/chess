a12088a98999badc9d1144812e156d4f13486a57
anthony ponzio <anthonyponzio1992@gmail.com>
2020-02-09 19:56:07 -0500

    add Pawn#at_start_row?

--- pieces/pawn.rb
+++ pieces/pawn.rb
@@ -4,4 +4,10 @@ class Pawn < Piece
   def symbol
     (color == :white) ? "♙" : "♟"
   end
+
+  def at_start_row?
+    row,_ = pos
+    (color == :black && row == 1) ||
+    (color == :white && row == 6)
+  end
 end
