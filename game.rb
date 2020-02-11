0439dab65cc2f011e3e46d90e0bbd2c804230964
anthony ponzio <anthonyponzio1992@gmail.com>
2020-02-11 02:59:20 -0500

    add Game#initialize

--- game.rb
+++ game.rb
@@ -1,2 +1,16 @@
+require_relative "board"
+require_relative "display"
+require_relative "human_player"
+
 class Game
+  def initialize
+    @board = Board.new
+    @display = Display.new(@board)
+    @players = [
+      HumanPlayer.new(:white, @display),
+      HumanPlayer.new(:black), @display),
+    ].shuffle
+
+    @current_player = @players.first
+  end
 end
